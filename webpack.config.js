6769809056909aac86b8b807fc1b8827e794c1d1
happy_hunter <gherciu553@gmail.com>
2019-02-03 14:31:47 +0200

    implimented webpack for graphhiql script

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode:'development',
+  entry: path.join(__dirname,'./src/common/graphiQl/index.jsx'),
+  output: {
+    path: path.join(__dirname, './dist/common/graphiQl'),
+    filename:'index.jsx',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+        {
+          test: /\.css$/,
+          use: [
+              "style-loader", // creates style nodes from JS strings
+              "css-loader", // translates CSS into CommonJS
+          ]
+      }
+    ]
+  }
+};
