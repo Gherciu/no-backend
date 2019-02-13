90a2ad3693270abc0e5da14d2b6059a56fb3486c
happy_hunter <gherciu553@gmail.com>
2019-02-13 19:17:21 +0200

    optimization of query buildeer

--- src/buildTablesSchema.js
+++ src/buildTablesSchema.js
@@ -7,10 +7,30 @@ import {
 
 import getFieldType from './helpers/getFieldType'
 import {tablesSuffix} from './helpers/strings'
+let buildTableRowType = (tableName,tableDesc,tablesRowsTypes) =>{
 
+    return new GraphQLObjectType({
+        name:tableName.substr(0,tableName.length-1),
+        description:'',
+        fields: () => {
+            let tableFields = {}
+            tableDesc.forEach((fieldObject) => {
+
+                if( new RegExp(/\_/ig).test(fieldObject.Field) ){//add table type to this field
+                    tableFields[`${fieldObject.Field.split('_')[0]}`] = {type: new GraphQLNonNull(tablesRowsTypes[`${fieldObject.Field.split('_')[0]}`].type)}
+                }else{//add graphQl type to this field
+                    tableFields[fieldObject.Field] = getFieldType(fieldObject.Type.toLowerCase(),fieldObject.Null.toLowerCase()==='yes' ? true : false)
+                }
+
+            })
+            return tableFields
+        },
+    })
+
+}
 const buildQueries = (tables) => {
-    let types    = {}//with args
-    let rawTypes = {}//without args
+    let types    = {}//tables types with args
+    let tablesRowsTypes = {}//tables row types
     const recursiveRegisterTableType = (tableName,tableDesc) => {
         if( new RegExp(/\_/ig).test(tableName) ){//if is a table with relation(one-to-many)
             console.log("o-t-m",tableName.split('_'))
@@ -20,13 +40,13 @@ const buildQueries = (tables) => {
 
                 let relationsFields = tableDesc.filter((item)=>new RegExp(/\_/ig).test(item.Field))
                 let hasAddedRelationsTablesToTypes = false
-                // relationsFields.forEach((relationFieldObjectItem)=>{//ceck if relation table is in types
+                relationsFields.forEach((relationFieldObjectItem)=>{//ceck if relation table is in types
 
-                //     if(types[`${relationFieldObjectItem.Field.split('_')[0]}${tablesSuffix}`]){
-                //         hasAddedRelationsTablesToTypes = true
-                //     }
+                    if(types[`${relationFieldObjectItem.Field.split('_')[0]}${tablesSuffix}`]){
+                        hasAddedRelationsTablesToTypes = true
+                    }
 
-                // })
+                })
                 if(relationsFields.length > 0 && !hasAddedRelationsTablesToTypes){//if has relations fields (many-to-one)
 
                     relationsFields.forEach((relationFieldObjectItem)=>{//add all relationsFields --> tables to types
@@ -40,34 +60,13 @@ const buildQueries = (tables) => {
                         recursiveRegisterTableType(`${relationFieldObjectItem.Field.split('_')[0]}${tablesSuffix}`,relationTableDesc)
 
                     })
-                    // recursiveRegisterTableType( tableName,tableDesc )//add table initiator to types 
+                    recursiveRegisterTableType( tableName,tableDesc )//add table initiator to types 
 
-                }else{//register table type to types and raw types
+                }else{//register table type to types and row types
 
-                    let tableType = (withArgs) =>{
-
-                        return new GraphQLObjectType({
-                            name:tableName,
-                            description:'',
-                            fields: () => {
-                                let tableFields = {}
-                                tableDesc.forEach((fieldObject) => {
-
-                                    if( new RegExp(/\_/ig).test(fieldObject.Field) ){//add table type to this field
-                                        // /tableFields[`${fieldObject.Field.split('_')[0]}${tablesSuffix}`] = rawTypes[`${fieldObject.Field.split('_')[0]}${tablesSuffix}`].type
-                                    }else{//add graphQl type to this field
-                                        tableFields[fieldObject.Field] = getFieldType(fieldObject.Type.toLowerCase(),fieldObject.Null.toLowerCase()==='yes' ? true : false)
-                                    }
-
-                                })
-                                return tableFields
-                            },
-                            args:null
-                        })
-
-                    }
-                    rawTypes[tableName] = {type :new GraphQLNonNull(new GraphQLList( tableType(false) ))}
-                    types[tableName]    = {type :new GraphQLNonNull(new GraphQLList( tableType(true) ))}
+                    tablesRowsTypes[tableName.substr(0,tableName.length-1)] = {type : buildTableRowType(tableName,tableDesc,tablesRowsTypes) }//singular row type
+                    types[tableName]    = {type :new GraphQLNonNull(new GraphQLList( tablesRowsTypes[tableName.substr(0,tableName.length-1)].type ))}//plural table type
+                
                 }
             }
 
